36a2963783c3e4cf8050a740f1082351d45261e0
Wzx <2309543755@qq.com>
2026-05-08 17:57:55 +0800

    fix:三次解决冲突

--- apps/web/src/apis/user/index.ts
+++ apps/web/src/apis/user/index.ts
@@ -12,14 +12,11 @@ export const registerApi = (data: UserRegister & { code: string }): Promise<Resp
 export const refreshTokenApi = (data: { refreshToken: string }): Promise<Response<Token>> => {
     return serverApi.post('/auth/refresh', data) as Promise<Response<Token>>
 }
-<<<<<<< HEAD
 //上传头像
 export const uploadAvatarApi = (file: FormData) => serverApi.post('/user/upload-avatar', file) as Promise<Response<AvatarResult>>
 //更新用户
 export const updateUser = (data: UserUpdate) => serverApi.post('/user/update-user', data) as Promise<Response<UserUpdate>>
-=======
 
 export const sendCodeApi = (phone: string): Promise<Response<null>> => {
     return serverApi.post('/auth/send-code', { phone }) as Promise<Response<null>>
 }
->>>>>>> 93102f4a7e42981493746188112dd1d0fd109910
